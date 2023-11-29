c0d0e543cd0548a251042c11e5e5cef02356f02d
NaingMinThant77 <naingminthant07@gmail.com>
2023-11-29 20:47:45 +0630

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/tabs.dart';
 
@@ -13,8 +12,7 @@ final theme = ThemeData(
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: App(),)
+    App(),
   );
 }
 
